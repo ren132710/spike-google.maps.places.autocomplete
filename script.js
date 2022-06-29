d8a91ccd299cd9ad18c27420adf8bf5eca9ead0e
ren132710 <ren132710@gmail.com>
2022-06-29 13:42:43 -0500

    clean up

--- script.js
+++ script.js
@@ -20,9 +20,12 @@
  *   </script>
  */
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
+
+//parcel-bundler seems to prefer this syntax for importing dotenv/config
+import {} from 'dotenv/config'
 
 const placeSearch = document.querySelector('[data-place-search]')
 const placeResult = document.querySelector('[data-place-result]')
